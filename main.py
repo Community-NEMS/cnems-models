bbec509eb452784c5ed97a9094a72c4fe69227b5
Jeff H <jeff@westernspark.us>
2026-09-18 18:08:03 -0700

    Cleanup of configs
    Adjust / clean config files to make 4 interesting runs for viewer.

--- main.py
+++ main.py
@@ -117,4 +117,5 @@ if __name__ == '__main__':
     main(common_config_path=Path(PROJECT_ROOT, 'run_configs/basic_elec_config.toml'))
     main(common_config_path=Path(PROJECT_ROOT, 'run_configs/exchange_elec_config.toml'))
     # # reduced string-named input set (input/electricity_light) with regional exchange
-    # main(common_config_path=Path(PROJECT_ROOT, 'run_configs/reduced_elec_config.toml'))
+    main(common_config_path=Path(PROJECT_ROOT, 'run_configs/reduced_elec_config.toml'))
+    main(common_config_path=Path(PROJECT_ROOT, 'run_configs/expandable_elec_config.toml'))
